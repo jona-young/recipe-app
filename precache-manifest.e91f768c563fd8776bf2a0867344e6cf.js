33a2ff52b5e4a3fab22bd65888f1669c86a7c5a1
jonayoung <young.jon@icloud.com>
2021-01-21 01:23:48 -0500

    Updates

---
+++ precache-manifest.e91f768c563fd8776bf2a0867344e6cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942983e4a130d1cfe112b443a0b231f3",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "ab2c0125d4f263702f1c",
+    "url": "/recipe-app/static/css/main.35816dda.chunk.css"
+  },
+  {
+    "revision": "f410316a14c63df5ede9",
+    "url": "/recipe-app/static/js/2.686d39f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipe-app/static/js/2.686d39f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab2c0125d4f263702f1c",
+    "url": "/recipe-app/static/js/main.e0418b3b.chunk.js"
+  },
+  {
+    "revision": "cd77bb1956273d314d45",
+    "url": "/recipe-app/static/js/runtime-main.14680ac4.js"
+  }
+]);
